bad5e74f1a7d5a27d17f17df142c3765d13a89e0
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-04-12 06:40:27 +0200

    Remove unused method

--- Sources/Source/EntityVersion.swift
+++ Sources/Source/EntityVersion.swift
@@ -1,13 +1,6 @@
 public enum EntityVersion {
-    case new
+    case notSaved
     case version(Int32)
-
-    public func get() throws -> Int32 {
-        switch self {
-        case .version(let version): return version
-        case .new: throw ReconstitutionError.incorrectType
-        }
-    }
 }
 
 extension EntityVersion: Equatable {}
