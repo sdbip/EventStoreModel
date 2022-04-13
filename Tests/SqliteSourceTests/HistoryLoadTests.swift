3c523b0b20d0df741996099d94ef3d317eb0f1b0
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-04-13 16:15:21 +0200

    Just check that the table exists

--- Tests/SqliteSourceTests/HistoryLoadTests.swift
+++ Tests/SqliteSourceTests/HistoryLoadTests.swift
@@ -28,47 +28,9 @@ final class HistoryLoadTests: XCTestCase {
         }
 
         var statement: OpaquePointer?
-
-        guard sqlite3_prepare_v2(db, """
-            insert into Entities (id, type, version)
-            values ('test', 'testtype', 42)
-            """, -1, &statement, nil) == SQLITE_OK else {
-            let errmsg = String(cString: sqlite3_errmsg(db)!)
-            return XCTFail("error preparing insert: \(errmsg)")
-        }
-
-        guard sqlite3_step(statement) == SQLITE_DONE else {
-            let errmsg = String(cString: sqlite3_errmsg(db)!)
-            return XCTFail("failure inserting foo: \(errmsg)")
-        }
-
-        guard sqlite3_finalize(statement) == SQLITE_OK else {
-            let errmsg = String(cString: sqlite3_errmsg(db)!)
-            return XCTFail("error finalizing prepared statement: \(errmsg)")
-        }
-
-        statement = nil
-
-        guard sqlite3_prepare_v2(db, "select id, type, version from Entities", -1, &statement, nil) == SQLITE_OK else {
+        guard sqlite3_prepare_v2(db, "select * from Entities", -1, &statement, nil) == SQLITE_OK else {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             return XCTFail("error preparing select: \(errmsg)")
-        }
-
-        while sqlite3_step(statement) == SQLITE_ROW {
-            let id = sqlite3_column_text(statement, 0)
-            let type = sqlite3_column_text(statement, 1)
-            let version = sqlite3_column_int64(statement, 2)
-
-            guard let id = id else {
-                return XCTFail("id is null")
-            }
-            XCTAssertEqual(String(cString: id), "test")
-
-            guard let type = type else {
-                return XCTFail("type is null")
-            }
-            XCTAssertEqual(String(cString: type), "testtype")
-            XCTAssertEqual(version, 42)
         }
 
         guard sqlite3_finalize(statement) == SQLITE_OK else {
