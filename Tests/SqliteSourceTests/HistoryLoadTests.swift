c67df3364be91016bf119299983b54bd67b68888
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-04-13 18:05:45 +0200

    Remove unused type

--- Tests/SqliteSourceTests/HistoryLoadTests.swift
+++ Tests/SqliteSourceTests/HistoryLoadTests.swift
@@ -49,19 +49,3 @@ final class HistoryLoadTests: XCTestCase {
         XCTAssertEqual(history.version, 42)
     }
 }
-
-final class TestEntity: Entity {
-    static let type = "TestEntity"
-    let version: EntityVersion
-    let unpublishedEvents: [UnpublishedEvent]
-    var lastReconstitutedEvent: PublishedEvent?
-
-    init(version: EntityVersion) {
-        self.version = version
-        unpublishedEvents = []
-    }
-
-    func apply(_ event: PublishedEvent) {
-        lastReconstitutedEvent = event
-    }
-}
