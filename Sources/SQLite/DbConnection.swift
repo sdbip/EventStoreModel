861dcfa4117dc9ff9beb15624187ea6c7566b9e3
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-04-13 16:58:26 +0200

    Add static factory method to SQLiteError

--- Sources/SQLite/DbConnection.swift
+++ Sources/SQLite/DbConnection.swift
@@ -5,29 +5,31 @@ public struct DbConnection {
 
     public init(openFile file: String) throws {
         var connection: OpaquePointer?
-        guard sqlite3_open(file, &connection) == SQLITE_OK,
-            let connection = connection else {
-            let errmsg = String(cString: sqlite3_errmsg(connection)!)
-            throw SQLiteError.message(errmsg)
+        guard sqlite3_open(file, &connection) == SQLITE_OK else {
+            throw SQLiteError.lastError(connection: connection)
         }
-        self.pointer = connection
+        self.pointer = connection!
     }
 
     public func execute(_ statement: String) throws {
         if sqlite3_exec(self.pointer, statement, nil, nil, nil) != SQLITE_OK {
-            let errmsg = String(cString: sqlite3_errmsg(self.pointer)!)
-            throw SQLiteError.message(errmsg)
+            throw SQLiteError.lastError(connection: self.pointer)
         }
     }
 
     public func close() throws {
         if sqlite3_close(self.pointer) != SQLITE_OK {
-            let errmsg = String(cString: sqlite3_errmsg(self.pointer)!)
-            throw SQLiteError.message(errmsg)
+            throw SQLiteError.lastError(connection: self.pointer)
         }
     }
 }
 
 enum SQLiteError: Error {
+    case unknown
     case message(String)
+
+    static func lastError(connection: OpaquePointer?) -> SQLiteError {
+        guard let msg = sqlite3_errmsg(connection) else { return .unknown }
+        return .message(String(cString: msg))
+    }
 }
