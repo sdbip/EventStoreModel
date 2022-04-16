3d668b67a12da73739524bbecef71ea4149a9027
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-04-16 15:35:44 +0200

    Extract code to extension

--- Sources/SqliteSource/EntityPublisher.swift
+++ Sources/SqliteSource/EntityPublisher.swift
@@ -14,30 +14,40 @@ public struct EntityPublisher {
         let connection = try Connection(openFile: dbFile)
 
         try connection.transaction {
-            let statement1 = try Statement(prepare: """
-                INSERT INTO Entities (id, type, version)
-                VALUES (?, ?, ?);
-                """,
-                connection: connection)
-            statement1.bind(entity.id, to: 1)
-            statement1.bind(EntityType.type, to: 2)
-            statement1.bind(0, to: 3)
-            try statement1.execute()
+            try connection.add(entity)
 
             for event in entity.unpublishedEvents {
-                let statement2 = try Statement(prepare: """
-                    INSERT INTO Events (entity, name, details, actor, version, position)
-                    VALUES (?, ?, ?, ?, ?, ?);
-                    """,
-                    connection: connection)
-                statement2.bind(entity.id, to: 1)
-                statement2.bind(event.name, to: 2)
-                statement2.bind(event.details, to: 3)
-                statement2.bind(actor, to: 4)
-                statement2.bind(0, to: 5)
-                statement2.bind(0, to: 6)
-                try statement2.execute()
+                try connection.publish(event, entityId: entity.id, actor: actor)
             }
         }
+    }
+}
+
+private extension Connection {
+    func add<EntityType>(_ entity: EntityType) throws where EntityType: Entity {
+        let statement = try Statement(prepare: """
+            INSERT INTO Entities (id, type, version)
+            VALUES (?, ?, ?);
+            """,
+            connection: self)
+        statement.bind(entity.id, to: 1)
+        statement.bind(EntityType.type, to: 2)
+        statement.bind(0, to: 3)
+        try statement.execute()
+    }
+
+    func publish(_ event: UnpublishedEvent, entityId: String, actor: String) throws {
+        let statement = try Statement(prepare: """
+            INSERT INTO Events (entity, name, details, actor, version, position)
+            VALUES (?, ?, ?, ?, ?, ?);
+            """,
+            connection: self)
+        statement.bind(entityId, to: 1)
+        statement.bind(event.name, to: 2)
+        statement.bind(event.details, to: 3)
+        statement.bind(actor, to: 4)
+        statement.bind(0, to: 5)
+        statement.bind(0, to: 6)
+        try statement.execute()
     }
 }
