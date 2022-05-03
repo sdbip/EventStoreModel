fcb373a21058c2dfbdb56518dff36312bfca93ef
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-03 07:22:42 +0200

    Extract duplication to private method

--- Sources/SqliteSource/EventPublisher.swift
+++ Sources/SqliteSource/EventPublisher.swift
@@ -11,18 +11,26 @@ public struct EventPublisher {
     }
 
     public func publishChanges<State>(entity: Entity<State>, actor: String) throws where State: EntityState {
+        try publish(events: entity.state.unpublishedEvents, entityId: entity.id, entityType: State.typeId, actor: actor) {
+            v in v == entity.version.value
+        }
+    }
+
+    public func publish(_ event: UnpublishedEvent, forId id: String, type: String, actor: String) throws {
+        try publish(events: [event], entityId: id, entityType: type, actor: actor, isExpectedVersion: { _ in true })
+    }
+
+    private func publish(events: [UnpublishedEvent], entityId: String, entityType: String, actor: String, isExpectedVersion: (Int32?) -> Bool) throws {
         let database = try Database(openFile: dbFile)
 
-        let events = entity.state.unpublishedEvents
-
         try database.transaction {
-            let currentVersion = try database.version(ofEntityWithId: entity.id)
-            guard currentVersion == entity.version.value else { throw SQLiteError.message("Concurrency Error") }
+            let currentVersion = try database.version(ofEntityWithId: entityId)
+            guard isExpectedVersion(currentVersion) != false else { throw SQLiteError.message("Concurrency Error") }
 
             if let currentVersion = currentVersion {
-                try database.updateVersion(ofEntityWithId: entity.id, to: Int32(events.count) + currentVersion)
+                try database.updateVersion(ofEntityWithId: entityId, to: Int32(events.count) + currentVersion)
             } else {
-                try database.addEntity(id: entity.id, type: State.typeId, version: Int32(events.count))
+                try database.addEntity(id: entityId, type: entityType, version: Int32(events.count))
             }
 
             var nextPosition = try database.nextPosition()
@@ -30,31 +38,16 @@ public struct EventPublisher {
 
             var nextVersion = (currentVersion ?? -1) + 1
             for event in events {
-                try database.insertEvent(entityId: entity.id, name: event.name, jsonDetails: event.jsonDetails, actor: actor, version: nextVersion, position: nextPosition)
+                try database.insertEvent(
+                    entityId: entityId,
+                    name: event.name,
+                    jsonDetails: event.jsonDetails,
+                    actor: actor,
+                    version: nextVersion,
+                    position: nextPosition)
                 nextVersion += 1
                 nextPosition += 1
             }
-        }
-    }
-
-    public func publish(_ event: UnpublishedEvent, forId id: String, type: String, actor: String) throws {
-
-        let database = try Database(openFile: dbFile)
-
-        try database.transaction {
-            let currentVersion = try database.version(ofEntityWithId: id)
-
-            if let currentVersion = currentVersion {
-                try database.updateVersion(ofEntityWithId: id, to: currentVersion + 1)
-            } else {
-                try database.addEntity(id: id, type: type, version: 1)
-            }
-
-            let nextPosition = try database.nextPosition()
-            try database.setPosition(nextPosition + 1)
-
-            let nextVersion = (currentVersion ?? -1) + 1
-            try database.insertEvent(entityId: id, name: event.name, jsonDetails: event.jsonDetails, actor: actor, version: nextVersion, position: nextPosition)
         }
     }
 }
