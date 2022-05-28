3f4dfab7848aba9e025028eabf9d2201b620db47
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-28 15:30:38 +0200

    Extract duplicated code to a function

--- Tests/PostgresSourceTests/HistoryLoadTests.swift
+++ Tests/PostgresSourceTests/HistoryLoadTests.swift
@@ -10,19 +10,7 @@ final class HistoryLoadTests: XCTestCase {
     var database: Database!
 
     override func setUp() async throws {
-        var noDbConfig = configuration
-        noDbConfig.database = ""
-        
-        let noDb = Database(connection: try Connection(configuration: noDbConfig))
-        try? noDb.operation("CREATE DATABASE \(configuration.database)").execute()
-
-        let connection = try Connection(configuration: configuration)
-        database = Database(connection: connection)
-        try Schema.add(to: database)
-        try database.operation(#"DELETE FROM "Events""#).execute()
-        try database.operation(#"DELETE FROM "Entities""#).execute()
-        try database.operation(#"UPDATE "Properties" SET "value" = 0 WHERE "name" = 'next_position'"#).execute()
-        
+        database = try setUpEmptyTestDatabase()
         store = EntityStore(repository: database)
     }
     
@@ -66,26 +54,5 @@ final class HistoryLoadTests: XCTestCase {
         dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS Z"
         return dateFormatter.string(from: date)
-    }
-
-    var configuration: ConnectionConfiguration {
-        var config = ConnectionConfiguration()
-        config.host = "localhost"
-        config.port = 5432
-        config.ssl = false
-
-        if let database = ProcessInfo.processInfo.environment["POSTGRES_TEST_DATABASE"] {
-            config.database = database
-        }
-
-        if let user = ProcessInfo.processInfo.environment["POSTGRES_TEST_USER"] {
-            config.user = user
-        }
-
-        if let password = ProcessInfo.processInfo.environment["POSTGRES_TEST_PASS"] {
-            config.credential = Credential.cleartextPassword(password: password)
-        }
-
-        return config
     }
 }
--- Tests/PostgresSourceTests/PublishingTests.swift
+++ Tests/PostgresSourceTests/PublishingTests.swift
@@ -10,19 +10,7 @@ final class PublishingTests: XCTestCase {
     var database: Database!
     
     override func setUp() async throws {
-        var noDbConfig = configuration
-        noDbConfig.database = ""
-        
-        let noDb = Database(connection: try Connection(configuration: noDbConfig))
-        try? noDb.operation("CREATE DATABASE \(configuration.database)").execute()
-
-        let connection = try Connection(configuration: configuration)
-        database = Database(connection: connection)
-        try Schema.add(to: database)
-        try database.operation(#"DELETE FROM "Events""#).execute()
-        try database.operation(#"DELETE FROM "Entities""#).execute()
-        try database.operation(#"UPDATE "Properties" SET "value" = 0 WHERE "name" = 'next_position'"#).execute()
-        
+        database = try setUpEmptyTestDatabase()
         publisher = EventPublisher(repository: database)
         entityStore = EntityStore(repository: database)
     }
@@ -124,28 +112,6 @@ final class PublishingTests: XCTestCase {
             #"SELECT MAX(position) FROM "Events" WHERE "entityId" = 'test'"#
         ).single { try Int64($0[0].int()) }
     }
-    
-    var configuration: ConnectionConfiguration {
-        var config = ConnectionConfiguration()
-        config.host = "localhost"
-        config.port = 5432
-        config.ssl = false
-
-        if let database = ProcessInfo.processInfo.environment["POSTGRES_TEST_DATABASE"] {
-            config.database = database
-        }
-
-        if let user = ProcessInfo.processInfo.environment["POSTGRES_TEST_USER"] {
-            config.user = user
-        }
-
-        if let password = ProcessInfo.processInfo.environment["POSTGRES_TEST_PASS"] {
-            config.credential = Credential.cleartextPassword(password: password)
-        }
-
-        return config
-    }
-
 }
 
 final class TestEntity: EntityState {
