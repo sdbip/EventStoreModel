f330b42d63cd2918b950e41d9818ae46b0854451
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-28 11:30:30 +0200

    Extract computed property

--- Tests/PostgresSourceTests/HistoryLoadTests.swift
+++ Tests/PostgresSourceTests/HistoryLoadTests.swift
@@ -4,19 +4,33 @@ import PostgresClientKit
 
 final class HistoryLoadTests: XCTestCase {
     func test_canConnect() throws {
-        var config = ConnectionConfiguration()
-        config.host = "localhost"
-        config.port = 5432
-        config.database = ProcessInfo.processInfo.environment["POSTGRES_TEST_DATABASE"] ?? ""
-        config.user = ProcessInfo.processInfo.environment["POSTGRES_TEST_USER"] ?? ""
-        config.ssl = false
-
-        let connection = try Connection(configuration: config)
+        let connection = try Connection(configuration: configuration)
         let statement = try connection.prepareStatement(text: "SELECT 1")
         let x = try statement.execute()
             .map { try $0.get().columns[0].int() }
             .first
-        
+
         XCTAssertEqual(x, 1)
+    }
+
+    var configuration: ConnectionConfiguration {
+        var config = ConnectionConfiguration()
+        config.host = "localhost"
+        config.port = 5432
+        config.ssl = false
+
+        if let database = ProcessInfo.processInfo.environment["POSTGRES_TEST_DATABASE"] {
+            config.database = database
+        }
+
+        if let user = ProcessInfo.processInfo.environment["POSTGRES_TEST_USER"] {
+            config.user = user
+        }
+
+        if let password = ProcessInfo.processInfo.environment["POSTGRES_TEST_PASS"] {
+            config.credential = Credential.cleartextPassword(password: password)
+        }
+
+        return config
     }
 }
