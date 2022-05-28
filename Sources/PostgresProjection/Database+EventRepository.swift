c1b80b0a9ce9eb6fbb6173166c51133fe2259b72
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-05-28 16:27:46 +0200

    Implement EventRepository

---
+++ Sources/PostgresProjection/Database+EventRepository.swift
@@ -0,0 +1,30 @@
+import Postgres
+import Projection
+
+extension Database: EventRepository {
+    public func readEvents(maxCount: Int, after position: Int64?) throws -> [Event] {
+        let operation = try operation("""
+            SELECT "entityId", "entityType", "name", "details", "position" FROM "Events" WHERE "position" > $1 LIMIT \(maxCount)
+            """,
+            parameters: Int(position ?? -1))
+        return try events(from: operation)
+    }
+
+    public func readEvents(at position: Int64) throws -> [Event] {
+        let operation = try operation("""
+            SELECT "entityId", "entityType", "name", "details", "position" FROM "Events" WHERE "position" = $1
+            """,
+            parameters: Int(position))
+        return try events(from: operation)
+    }
+
+    private func events(from operation: Operation) throws -> [Event] {
+        return try operation.query {
+            return try Event(
+                entity: Entity(id: $0[0].string(), type: $0[1].string()),
+                name: $0[2].string(),
+                details: $0[3].string(),
+                position: Int64($0[4].int()))
+        }
+    }
+}
--- Tests/PostgresProjectionTests/PostgresEventRepositoryTests.swift
+++ Tests/PostgresProjectionTests/PostgresEventRepositoryTests.swift
@@ -1,1 +1,102 @@
+import XCTest
 
+import Postgres
+import PostgresProjection
+import PostgresSource
+import Projection
+
+final class PostgresEventRepositoryTests: XCTestCase {
+    var database: Database!
+
+    override func setUp() async throws {
+        database = try setUpEmptyTestDatabase()
+    }
+
+    func test_readEventsAt_returnsEvents() throws {
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 1, position: 1)
+
+        let events = try database.readEvents(at: 1)
+
+        XCTAssertEqual(events,
+            [Event(
+                entity: Entity(id: "entity", type: "type"),
+                name: "name",
+                details: "{}",
+                position: 1
+            )])
+    }
+
+    func test_readEventsAt_returnsOnlyEventsAtTheIndicatedPosition() throws {
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 0, position: 0)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 1, position: 1)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 2, position: 2)
+
+        let events = try database.readEvents(at: 1)
+
+        XCTAssert(events.allSatisfy({ $0.position == 1 }))
+
+    }
+
+    func test_readEventsAfter_returnsEvents() throws {
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 1, position: 1)
+
+        let events = try database.readEvents(maxCount: 1, after: 0)
+
+        XCTAssertEqual(events,
+            [Event(
+                entity: Entity(id: "entity", type: "type"),
+                name: "name",
+                details: "{}",
+                position: 1
+            )])
+    }
+
+    func test_readEventsAfter_returnsOnlyEventsAtLaterPositions() throws {
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 0, position: 0)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 1, position: 1)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 2, position: 2)
+
+        let events = try database.readEvents(maxCount: 3, after: 0)
+
+        XCTAssertEqual(events.map { $0.position }, [1, 2])
+    }
+
+    func test_readEventsAfter_returnsAllEventsWhenNoPositionSpecified() throws {
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 0, position: 0)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 1, position: 1)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 2, position: 2)
+
+        let events = try database.readEvents(maxCount: 3, after: nil)
+
+        XCTAssertEqual(events.map { $0.position }, [0, 1, 2])
+    }
+
+    func test_readEventsFromBeginning_returnsNoMoreThanMaxCountEvents() throws {
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 0, position: 0)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 1, position: 1)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 2, position: 2)
+
+        let events = try database.readEvents(maxCount: 2, after: nil)
+
+        XCTAssertEqual(events.map { $0.position }, [0, 1])
+    }
+
+    func test_readEventsAfter_returnsNoMoreThanMaxCountEvents() throws {
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 0, position: 0)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 1, position: 1)
+        try database.insertEventRow(entityId: "entity", entityType: "type", name: "name", jsonDetails: "{}", actor: "actor", version: 2, position: 2)
+
+        let events = try database.readEvents(maxCount: 1, after: 0)
+
+        XCTAssertEqual(events.map { $0.position }, [1])
+    }
+}
+
+extension Event: Equatable {
+    public static func ==(left: Event, right: Event) -> Bool {
+        return left.entity == right.entity &&
+        left.name == right.name &&
+        left.jsonDetails == right.jsonDetails &&
+        left.position == right.position
+    }
+}
